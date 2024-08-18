bb310860478e745877f8ff6d534eee4fae7edf42
jeremyperwira <jeremyperwira@protonmail.com>
2024-08-18 14:06:46 +0700

    del: old profile

--- src/app/profile/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Dashboard } from "@/components/Profile/menu";
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Dashboard>{children}</Dashboard>;
-}
