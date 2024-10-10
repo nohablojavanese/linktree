0b761ac273b58541b6f7fb1867ebed93a86a5e4f
jeremyperwira <jeremyperwira@protonmail.com>
2024-10-10 17:45:39 +0700

    Greetings

--- src/components/Profile/PCNavClient.tsx
+++ src/components/Profile/PCNavClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, ExternalLink, LogOut } from "lucide-react";
 import {
   Dropdown,
   DropdownTrigger,
@@ -13,6 +13,8 @@ import { Profile } from "@/lib/types/type";
 import { navItems } from "./navItems";
 import { signOut } from "@/lib/supabase/sign";
 import { useRouter } from "next/navigation";
+import { SimpleThemeSwitcher } from "../DarkMode";
+import { getGreeting } from "@/lib/helper/greeting";
 
 interface PCNavClientProps {
   user: Profile;
@@ -31,7 +33,7 @@ const PCNavClient: React.FC<PCNavClientProps> = ({ user }) => {
 
   return (
     <aside
-      className={`fixed hidden inset-y-0 left-0 z-10 md:flex flex-col text-black bg-white dark:bg-gray-900 border-r transition-all duration-300 ${
+      className={`fixed hidden inset-y-0 left-0 z-10 md:flex flex-col text-black dark:text-white bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 transition-all duration-300 ${
         isExpanded ? "w-64" : "w-16"
       }`}
     >
@@ -48,31 +50,62 @@ const PCNavClient: React.FC<PCNavClientProps> = ({ user }) => {
       <div className="mt-auto mb-4 px-4">
         <Dropdown placement="top-start" backdrop="blur">
           <DropdownTrigger>
-            <button className="flex items-center w-full text-black">
+            <button className="flex items-center w-full text-black dark:text-white">
               <Avatar
                 isBordered
                 size="sm"
                 src={user.image_url || "https://i.pravatar.cc/150"}
                 className="transition-transform"
               />
-              {isExpanded && (  
+              {isExpanded && (
                 <span className="ml-3 text-sm font-medium truncate">
                   {user.username}
                 </span>
               )}
             </button>
           </DropdownTrigger>
-          <DropdownMenu aria-label="User menu actions" variant="flat" className="text-black">
+          <DropdownMenu
+            aria-label="User menu actions"
+            variant="flat"
+            className="text-black dark:text-white "
+          >
             <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">@{user.username}</p>
+              <p className="font-semibold">
+                {getGreeting({ language: "en" })},
+              </p>
+              <p className="text-sm text-gray-500">{user.username}</p>
             </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="visit-site">Visit Site</DropdownItem>
+            {/* <DropdownItem key="settings">My Settings</DropdownItem> */}
+            <DropdownItem
+              key="visit-site"
+              onPress={() =>
+                window.open(
+                  `${process.env.NEXT_PUBLIC_URL}/${user.username}`,
+                  "_blank"
+                )
+              }
+            >
+              <div className="flex items-center">
+                <span>Visit Site</span>
+                <ExternalLink className="w-4 h-4 ml-1" />
+              </div>
+            </DropdownItem>
             <DropdownItem key="terms">Terms</DropdownItem>
             <DropdownItem key="privacy">Privacy Policy</DropdownItem>
-            <DropdownItem key="logout" color="danger" onPress={handleSignOut}>
-              Log Out
+            <DropdownItem key="theme">
+              <SimpleThemeSwitcher />
+            </DropdownItem>
+
+            <DropdownItem
+              key="logout"
+              color="danger"
+              className="bg-red-100 dark:bg-red-900 text-red-600 dark:text-red-300"
+              onPress={handleSignOut}
+            >
+              <div className="flex items-center">
+                <LogOut className="w-4 h-4" />
+                <span className="ml-2">Log Out</span>
+              </div>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
@@ -80,7 +113,8 @@ const PCNavClient: React.FC<PCNavClientProps> = ({ user }) => {
 
       <button
         onClick={() => setIsExpanded(!isExpanded)}
-        className="absolute top-1/2 -right-3 text-blue-500 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-full p-1 transform -translate-y-1/2"
+        className="absolute top-1/2 -right-3 text-blue-500 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-full p-1 transform -translate-y-1/2"
+        aria-label={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
       >
         {isExpanded ? (
           <ChevronLeft className="w-4 h-4" />
